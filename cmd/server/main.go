5e817cc3d38d887adf79999fa018df812fa26201
antoniogisondi <anto02.gisondi@gmail.com>
2026-05-06 14:08:30 +0200

    init

---
+++ cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "MGL Safety Manager API",
+			"status":  "online",
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
