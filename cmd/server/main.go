123227cf775a4f69b71ef1fc4382312fb661f24b
Antonio Gisondi <anto02.gisondi@gmail.com>
2026-05-06 19:21:55 +0200

    connect db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,15 +4,26 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/joho/godotenv"
+
+	"github.com/antoniogisondi/mgl-safety-cloud-backend/internal/database"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Errore caricamento .env")
+	}
+
+	database.Connect()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "MGL Safety Manager API",
-			"status":  "online",
+			"message": "API Online",
 		})
 	})
 
---
+++ internal/database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Errore connessione database: ", err)
+	}
+
+	DB = db
+
+	log.Println("Database Supabase connesso correttamente")
+}
